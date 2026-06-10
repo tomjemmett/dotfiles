4e0e337622ae82ff65990eb0050c09d6250671ef
Tom Jemmett <thomas.jemmett@nhs.net>
2026-06-10 13:33:31 +0100

    updates dotfiles

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -16,16 +16,22 @@
 
 -- See https://wiki.hypr.land/Configuring/Monitors/
 hl.monitor({
-    output="DP-1",
+    output="desc:Dell Inc. DELL U2723QE HFVHZG3",
     mode="3840x2160@60.00",
     position="0x0",
     scale=1.250
 })
 hl.monitor({
-    output="HDMI-A-2",
+    output="desc:Dell Inc. DELL U2720Q D4G0Y13",
     mode="3840x2160@60.00",
     position="3072x0",
     scale=1.250
+})
+hl.monitor({
+    output="desc:LG Electronics LG TV 0x01010101",
+    mode="1920@1080@x60.00",
+    position="2112x-1080",
+    scale=1.0
 })
 
 ---------------------
@@ -44,7 +50,7 @@ local browser = "firefox"
 -------------------
 
 hl.on("hyprland.start", function ()
-    hl.exec_cmd("~/Workspace/Waybar/build/waybar")
+    hl.exec_cmd("~/workspace/Waybar/build/waybar")
     hl.exec_cmd("hyprpaper")
     hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=Hyprland")
     hl.exec_cmd("systemctl --user start hyprpolkitagent")
@@ -245,7 +251,8 @@ hl.gesture({
 
 hl.device({
     name = "logitech-usb-receiver-mouse",
-    natural_scroll = true
+    -- natural_scroll = true
+    natural_scroll = false
 })
 
 
